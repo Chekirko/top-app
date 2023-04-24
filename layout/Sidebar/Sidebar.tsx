213cb7340d4a034664b29b5d893338f9e5a39bdb
chekirko <victorche59@gmail.com>
2023-04-24 16:23:35 +0300

    add Logo and Sidebar without search input

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -3,10 +3,16 @@ import { SidebarProps } from "./Sidebar.props";
 import styles from "./Sidebar.module.css";
 import cn from "classnames";
 import Menu from "../Menu/Menu";
+import Logo from "../logo.svg";
 
-export default function Sidebar({ ...props }: SidebarProps): JSX.Element {
+export default function Sidebar({
+  className,
+  ...props
+}: SidebarProps): JSX.Element {
   return (
-    <div {...props}>
+    <div className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <div>Search</div>
       <Menu />
     </div>
   );
