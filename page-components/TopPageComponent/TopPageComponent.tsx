fd3f3e94070ed5ffd048d4a3feec90a28ae3f531
chekirko <victorche59@gmail.com>
2023-04-26 14:11:44 +0300

    create TopPageComponent and add HhData component

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,9 +1,44 @@
+import styles from "./TopPageComponent.module.css";
+import Htag from "@/components/Htag/Htag";
 import { TopPageProps } from "./TopPageComponent.props";
+import Tag from "@/components/Tag/Tag";
+import Card from "@/components/Card/Card";
+import Hhdata from "@/components/Hhdata/Hhdata";
+import { TopLevelCategory } from "../../interfaces/page.interface";
 
 export default function TopPageComponent({
   page,
   products,
   firstCategory,
 }: TopPageProps): JSX.Element {
-  return <>{products && products.length}</>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag="h1">{page.title}</Htag>
+        {products && (
+          <Tag size="m" color="grey">
+            {products.length}
+          </Tag>
+        )}
+        <span>Sorting</span>
+      </div>
+
+      <div>
+        {products && products.map((p) => <div key={p._id}>{p.title}</div>)}
+      </div>
+
+      <div className={styles.hhTitle}>
+        <Htag tag="h2">Вакансии - {page.category}</Htag>
+        {products && (
+          <Tag size="m" color="red">
+            hh.ru
+          </Tag>
+        )}
+      </div>
+
+      {firstCategory === TopLevelCategory.Courses && (
+        <Hhdata {...page.hh}></Hhdata>
+      )}
+    </div>
+  );
 }
