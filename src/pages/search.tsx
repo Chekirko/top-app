24d0a7c072847c4a784c9f6782d0a917e1818ef7
chekirko <victorche59@gmail.com>
2023-06-08 16:37:14 +0300

    fix search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,46 +1,12 @@
 import { GetStaticProps } from "next";
-import Tag from "@/components/Tag/Tag";
 import { withLayout } from "../../layout/Layout";
 import { MenuItem } from "../../interfaces/menu.interface";
 import axios from "axios";
 import { API } from "../../helpers/api";
+import Htag from "@/components/Htag/Htag";
 
 function Search({ menu, firstCategory }: SearchProps): JSX.Element {
-  return (
-    <>
-      <Tag size="m" color="red">
-        Large red
-      </Tag>
-      <Tag size="m" color="green">
-        Large green
-      </Tag>
-      <Tag size="m" color="ghost">
-        Large ghost
-      </Tag>
-      <Tag size="m" color="accent">
-        Large accent
-      </Tag>
-      <Tag size="m" color="grey">
-        Large grey
-      </Tag>
-      <h1>Lets find something !!!</h1>
-      <Tag size="m" color="red">
-        Large red
-      </Tag>
-      <Tag size="m" color="green">
-        Large green
-      </Tag>
-      <Tag size="m" color="ghost">
-        Large ghost
-      </Tag>
-      <Tag size="m" color="accent">
-        Large accent
-      </Tag>
-      <Tag size="m" color="grey">
-        Large grey
-      </Tag>
-    </>
-  );
+  return <>{<Htag tag="h1">Страница поиска</Htag>}</>;
 }
 
 export default withLayout(Search);
